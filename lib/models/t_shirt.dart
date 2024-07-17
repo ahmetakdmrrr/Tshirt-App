ccd40d2c494849dd27c96d434c9978260d16875e
Ahmet Akdemir <ahmet551977@gmail.com>
2024-07-17 10:07:20 +0300

    introduction of api

--- lib/models/t_shirt.dart
+++ lib/models/t_shirt.dart
@@ -1,4 +1,5 @@
 import 'package:uuid/uuid.dart';
+
 const uuid = Uuid();
 
 class Tshirt {
@@ -9,13 +10,34 @@ class Tshirt {
     required this.description,
     required this.color,
     required this.size,
-  }):id = uuid.v4();
+  }) : id = uuid.v4();
 
-  final String id;
-  final String name;
-  final String description;
-  final String imagePath;
-  final double price;
-  final String size;
-  final List<String> color;
+  late String id;
+  late String name;
+  late String description;
+  late String imagePath;
+  late double price;
+  late String size;
+  late List<String> color;
+
+  Tshirt.fromjson(Map json) {
+    id = json["id"];
+    name = json['name'];
+    price = json['price'];
+    imagePath = json['imagePath'];
+    description = json['description'];
+    size = json['size'];
+    color = json['color'];
+  }
+  Map json() {
+    return {
+      "id": id,
+      "name": name,
+      "price": price,
+      "imagePath": imagePath,
+      "description": description,
+      "size": size,
+      "color": color,
+    };
+  }
 }
--- lib/models/users.dart
+++ lib/models/users.dart
@@ -17,17 +17,43 @@ class Users {
       required this.phone})
       : id = uuid.v4();
 
-  final String id;
-  final String name;
-  final String surName;
-  final String userName;
-  final DateTime date;
-  final String cinsiyet;
-  final String email;
-  final String password;
-  final String phone;
+  late String id;
+  late String name;
+  late String surName;
+  late String userName;
+  late DateTime date;
+  late String cinsiyet;
+  late String email;
+  late String password;
+  late String phone;
 
   String get formattedDate {
     return formatter.format(date);
+  }
+
+  Users.fromjson(Map json) {
+    id = json["id"];
+    name = json["name"];
+    surName = json["surName"];
+    userName = json["userName"];
+    date = json["date"].toDate();
+    cinsiyet = json["cinsiyet"];
+    email = json["email"];
+    password = json["password"];
+    phone = json["phone"];
+
+  }
+  Map json() {
+    return {
+      "id": id,
+      "name": name,
+      "surName": surName,
+      "useName" : userName,
+      "date" : date,
+      "cinsiyet":cinsiyet,
+      "email": email,
+      "password": password,
+      "phone": phone
+    };
   }
 }
